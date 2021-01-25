1e24da5f267fe0f101279e4606c01b2a22c92ef2
Javier Quiroga Almeida <quiroga218@gmail.com>
2021-01-24 19:56:57 -0600

    Fea: Interested Job for practicing
    One the most common Feature ever made

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+
+use MyApp\Data\Database as DB;
+
+new DB;
+
+
---
+++ src/data/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MyApp\Data;
+
+class Database {
+
+
+        public function __construct(){
+            echo 'Creating a database management';
+        }
+
+}
